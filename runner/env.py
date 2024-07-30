6a511b7e5da443eff299f89da9f17d021e3bfe39
utkarsh-dandagavhal <utkarsh.dandagavhal@indexnine.com>
2024-07-30 14:45:26 +0530

    Commit changes

---
+++ runner/env.py
@@ -0,0 +1,11 @@
+data = {
+    "URL": "",
+    "Environment": "",
+    "Browser": "",
+    "Options": [
+        "Git"
+    ],
+    "clone_path": "d://oats\\Execution-2024-07-30_14-43-05",
+    "EmailInput": [],
+    "IPInput": []
+}

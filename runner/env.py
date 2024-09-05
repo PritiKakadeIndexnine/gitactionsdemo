f08fe9f4bfd92eb9d99677fe05544fb8d2209c24
malaymahajan <malay.mahajan@gmail.com>
2024-09-05 15:09:05 +0530

    Commit changes

---
+++ runner/env.py
@@ -0,0 +1,11 @@
+data = {
+    "URL": "",
+    "Environment": "staging",
+    "Browser": "null",
+    "Options": [
+        "Git"
+    ],
+    "clone_path": "C:\\Users\\Malay\\Desktop\\acctest\\Execution-2024-09-05_15-05-03",
+    "EmailInput": [],
+    "IPInput": []
+}
